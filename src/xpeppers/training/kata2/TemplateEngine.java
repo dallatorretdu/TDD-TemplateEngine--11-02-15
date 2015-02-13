afb616064dc47f0b6047d9361351f0fe039fced2
Pietro Di Bello <orione-team+dip@xpeppers.com>
2015-02-13 15:15:41 +0100

    merge wrong

--- src/xpeppers/training/kata2/TemplateEngine.java
+++ src/xpeppers/training/kata2/TemplateEngine.java
@@ -7,27 +7,30 @@ class TemplateEngine {
 	public TemplateEngine() {}
 	
 	public String evaluate(String input, MapOfValues variableMap){
-		input = doublesBracketsWhenPreceededByDollar(input, variableMap);
-		input = parseSimpleVariablesInText(input, variableMap);
+		input = parseComplexVariablesInText(input, variableMap, "\\$\\{\\$", "\\}");
+		input = parseComplexVariablesInText(input, variableMap,    "\\{\\$", "\\}");
 		return input;
 	}
 	//
 	//SUBS
 	//
-	private String parseSimpleVariablesInText(String input, MapOfValues variableMap) {
+	
+	private String parseComplexVariablesInText(String input, MapOfValues variableMap, String prefix, String suffix, Boolean replaceValue) {
 		variableMap.initializeIterator();
 		
 		while(variableMap.hasNext()){
 			String varName = variableMap.getNextElement();
 			
-			String variableRegex = "\\{\\$" + varName + "\\}";
+			String variableRegex = prefix + varName + suffix;
+			
 			String replaceValue = variableMap.getVariable(varName);
 			
 			Pattern pattern = Pattern.compile(variableRegex);
 	        Matcher matcher = pattern.matcher(input);
-	        if(!matcher.find())
-	        	throw new IllegalArgumentException("Missing Value Exception");
-	        input = matcher.replaceAll(replaceValue);
+	        if(matcher.find())
+	        	input = matcher.replaceAll(replaceValue);
+	       // else
+	        //	throw new IllegalArgumentException("Missing Value Exception");
 		}
 		return input;
 	}
